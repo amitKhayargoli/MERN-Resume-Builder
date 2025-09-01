2a92c7b49c36e68576aa2b2186ee356e926abbc2
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-01 20:42:29 +0545

    frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage/LandingPage'
+import Layout from './pages/LandingPage/Layout'
+import ResumeTemplates from './pages/LandingPage/ResumeTemplates'
 
 const App = () => {
   return (
-     <h1 class="text-3xl">
-    EasyResume
-  </h1>
+    <BrowserRouter>
+      <Routes>
+    <Route element={<Layout />}>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/templates" element={<ResumeTemplates />} />
+    </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
