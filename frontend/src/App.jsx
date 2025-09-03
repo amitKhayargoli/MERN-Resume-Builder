933a9c0537886957c17a43ecb9e0f7270a538d53
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-03 22:29:19 +0545

    login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './pages/LandingPage/LandingPage'
-import Layout from './pages/LandingPage/Layout'
-import ResumeTemplates from './pages/LandingPage/ResumeTemplates'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Layout from "./pages/LandingPage/Layout";
+import ResumeTemplates from "./pages/LandingPage/ResumeTemplates";
+import Login from "./pages/Auth/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-    <Route element={<Layout />}>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/templates" element={<ResumeTemplates />} />
-    </Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/templates" element={<ResumeTemplates />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
