1ace68c0667eebcb9b1207e94ec8f191982f975f
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-09-04 00:33:01 +0545

    changes

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -2,12 +2,7 @@ import LoginCard from "./LoginCard";
 
 export default function Login() {
   return (
-    <section className="relative overflow-hidden flex-1 h-screen bg-[#F1EAFE] flex flex-col items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl md:text-5xl font-semibold text-slate-900">
-          Sign in to your account
-        </h1>
-      </div>
+    <section className="relative overflow-hidden flex-1 h-screen bg-[#F1EAFE] flex flex-col items-center">
       {/* Auth card */}
       <LoginCard />
     </section>
--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -3,12 +3,7 @@ import SignUpCard from "./SignUpCard";
 
 const Signup = () => {
   return (
-    <section className="relative overflow-hidden flex-1 h-screen bg-[#F1EAFE] flex flex-col items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl md:text-5xl font-semibold text-slate-900">
-          Create a new account
-        </h1>
-      </div>
+    <section className="relative overflow-hidden flex-1 h-screen bg-[#F1EAFE] flex flex-col items-center">
       {/* Auth card */}
       <SignUpCard />
     </section>
